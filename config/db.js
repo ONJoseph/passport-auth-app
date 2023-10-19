446ed10ab5e9a6938dcb0a60b73f1b5f22aa63d8
ONJoseph <on.josephinfo@gmail.com>
2023-10-19 14:49:07 +0100

    Added code

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('passport-auth-appdb', 'postgres', '2023', {
+const sequelize = new Sequelize('passport-auth-appdb', 'postgres', '****', {
   host: 'localhost',
   dialect: 'postgres',
 });
